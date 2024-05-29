2daffb3e176e4840571cead24eadffb160b3a7ef
Simon <ferlat.simon@gmail.com>
2024-05-29 14:18:50 +0200

    remove unecessary code & add comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { QueryClient, QueryClientProvider } from 'react-query';
 import './App.css';
 
 function App() {
 
-  const queryClient = new QueryClient()
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <a href='/users'>User List</a>
-    </QueryClientProvider>
+    <a href='/users'>User List</a>
   )
 }
 
--- src/hooks/useUsersData.tsx
+++ src/hooks/useUsersData.tsx
@@ -37,6 +37,22 @@ async function fetchUsers(page: number) {
 }
 
 export default function useUsersData(page: number) {
+
+    //Caching des Données:
+    // Par défaut, React Query met en cache les données pendant 5 minutes (300000 ms). Cela signifie que si vous refaites la même requête dans ce délai, les données mises en cache seront utilisées au lieu de faire un nouvel appel réseau.
+  
+    // Refetch on Window Focus:
+    // Par défaut, React Query va refaire la requête lorsque la fenêtre du navigateur regagne le focus. Cela permet de s'assurer que les données affichées sont à jour lorsque l'utilisateur revient sur l'application.
+    
+    // Stale Time:
+    // Le staleTime par défaut est de 0 ms. Cela signifie que dès que les données sont récupérées, elles sont immédiatement considérées comme "périmées" (stale). Cependant, grâce à la mise en cache, elles peuvent toujours être utilisées jusqu'à expiration du cache.
+    
+    // Retry:
+    // Par défaut, React Query va réessayer la requête 3 fois en cas d'échec, avec des délais croissants entre chaque tentative.
+    
+    // Refetch on Mount:
+    // Par défaut, React Query refait la requête à chaque fois que le composant est monté si les données sont considérées comme "périmées".
+
     const { data: users, isLoading: loading, error } = useQuery(['users', page], () => fetchUsers(page));
 
     if (error) {
